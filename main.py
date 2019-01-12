dc1666d171def30631871d92d8632699ac98d350
Lily Ou <lilyou@cs.ucsb.edu>
2019-01-11 23:36:23 -0800

    homepage up

---
+++ main.py
@@ -0,0 +1,26 @@
+import webapp2
+import jinja2
+import os
+import logging
+import time
+
+from google.appengine.ext import ndb
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template = env.get_template("templates/home.html")
+        self.response.write(template.render())
+
+    def post(self):
+        name = self.request.get("name")
+        self.redirect("/")
+
+
+app = webapp2.WSGIApplication([
+    ("/", MainPage),
+], debug=True)
