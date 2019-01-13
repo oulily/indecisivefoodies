0e85960391745608aeb8a5e95e23510743376978
Samhita Honnavalli <shonnavalli@169-231-171-37.wireless.ucsb.edu>
2019-01-12 21:48:19 -0800

    join

--- javascript/map.js
+++ javascript/map.js
@@ -3,38 +3,26 @@ var currentLocation;
 
 function getPlacesNearby(){
   console.log('getPlacesNearby')
-
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
-<<<<<<< HEAD
-=======
-
->>>>>>> fb3f6c87c9e44b0b083f359142acdfa27604e5d4
   var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (request.readyState == 4 && request.status == 200) {
+  request.onreadystatechange = function()
+  {
+    if (request.readyState == 4 && request.status == 200)
+    {
       var resp = JSON.parse(request.response);
       console.log(resp.results.name);
-      onPlacesSuccess(resp);
+
     }
   }
-<<<<<<< HEAD
-
-
-  request.open('GET', proxyurl+"https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+isla+vista&key=AIzaSyCjBjBHM4KRgn5WDcF_f4RxCRAGTT4dsr4");
-  console.log('sending request to places')
-  request.send();
-
-=======
 
 
 
-  // header("Access-Control-Allow-Credentials:true");
 
   request.open("GET", proxyurl + "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+isla+vista&key=AIzaSyCjBjBHM4KRgn5WDcF_f4RxCRAGTT4dsr4");
 
   console.log('sending request to places')
   request.send();
->>>>>>> fb3f6c87c9e44b0b083f359142acdfa27604e5d4
+
 }
 
 getPlacesNearby();
