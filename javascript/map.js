fb3f6c87c9e44b0b083f359142acdfa27604e5d4
Lily Ou <lilyou@cs.ucsb.edu>
2019-01-12 20:56:55 -0800

    fixed corb

--- javascript/map.js
+++ javascript/map.js
@@ -6,7 +6,6 @@ function getPlacesNearby(){
 
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
-  xhttp.open("GET", proxyurl + "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+isla+vista&key=AIzaSyCjBjBHM4KRgn5WDcF_f4RxCRAGTT4dsr4");
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
@@ -20,10 +19,8 @@ function getPlacesNearby(){
 
   // header("Access-Control-Allow-Credentials:true");
 
-  request.open('GET', url);
-  request.setRequestHeader("Access-Control-Allow-Origin", "*");
-  request.setRequestHeader("Access-Control-Allow-Methods", "GET,POST");
-  request.setRequestHeader("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
+  request.open("GET", proxyurl + "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+isla+vista&key=AIzaSyCjBjBHM4KRgn5WDcF_f4RxCRAGTT4dsr4");
+
   console.log('sending request to places')
   request.send();
 }
