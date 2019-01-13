bf16e02c45f0bf73b65def645c522f0148833e6e
Lily Ou <lilyou@cs.ucsb.edu>
2019-01-12 20:07:25 -0800

    results handler created

--- javascript/map.js
+++ javascript/map.js
@@ -6,13 +6,13 @@ window.onload = function(){
 
 function getPlacesNearby(){
   console.log('getPlacesNearby')
-  header("Access-Control-Allow-Origin:*");
-  header("Access-Control-Allow-Methods:GET,POST");
-  header("Access-Control-Allow-Headers:Content-Type");
-  header("Access-Control-Allow-Credentials:true");
+  // header("Access-Control-Allow-Origin:*");
+  // header("Access-Control-Allow-Methods:GET,POST");
+  // header("Access-Control-Allow-Headers:Content-Type");
+  // header("Access-Control-Allow-Credentials:true");
 
   var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+isla+vista&key=AIzaSyCjBjBHM4KRgn5WDcF_f4RxCRAGTT4dsr4';
-  var server = 'http://localhost:8080/?url='+ encodeURIComponent(url);
+  //var server = 'http://localhost:8080/?url='+ encodeURIComponent(url);
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
@@ -21,7 +21,7 @@ function getPlacesNearby(){
       onPlacesSuccess(resp);
     }
   }
-  request.open('GET', server);
+  request.open('GET', url);
   console.log('sending request to places')
   request.send();
   var service = new google.maps.places.PlacesService(map);
