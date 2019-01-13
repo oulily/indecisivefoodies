54b25caf601e81c8870f06fea04d2e1c5c9282f3
Lily Ou <lilyou@cs.ucsb.edu>
2019-01-12 20:19:55 -0800

    results handler created

--- javascript/map.js
+++ javascript/map.js
@@ -12,7 +12,6 @@ function getPlacesNearby(){
   // header("Access-Control-Allow-Credentials:true");
 
   var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+isla+vista&key=AIzaSyCjBjBHM4KRgn5WDcF_f4RxCRAGTT4dsr4';
-  //var server = 'http://localhost:8080/?url='+ encodeURIComponent(url);
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status == 200) {
